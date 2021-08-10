4f08647fe4b03cef4f0d8f23a54979350641fae8
Luis Silva <luismiguel.s91@gmail.com>
2021-08-10 10:25:12 +0300

    Fix the code

--- packages/react-app/scripts/ipfs.js
+++ packages/react-app/scripts/ipfs.js
@@ -1,15 +1,15 @@
-const chalk = require("chalk");
-const ipfsApi = require("ipfs-http-client");
-const { clearLine } = require("readline");
+const chalk = require('chalk');
+const ipfsApi = require('ipfs-http-client');
+const { clearLine } = require('readline');
 
 const { globSource } = ipfsApi;
 
-const infura = { host: "ipfs.infura.io", port: "5001", protocol: "https" };
+const infura = { host: 'ipfs.infura.io', port: '5001', protocol: 'https' };
 // Run your own ipfs daemon: https://docs.ipfs.io/how-to/command-line-quick-start/#install-ipfs
 // const localhost = { host: "localhost", port: "5001", protocol: "http" };
 
 const ipfs = ipfsApi(infura);
-const ipfsGateway = "https://ipfs.io/ipfs/";
+const ipfsGateway = 'https://ipfs.io/ipfs/';
 
 const addOptions = {
   pin: true,
@@ -26,7 +26,7 @@ async function pushDirectoryToIpfs(path) {
 
 async function publishHashToIpns(ipfsHash) {
   try {
-    const response = await ipfs.name.publish(`/ipfs/${ipfsHash}`);
+    const response = await ipfs.name.publish(`/ipfs/${ ipfsHash }`);
     return response;
   } catch (e) {
     return {};
@@ -36,60 +36,58 @@ async function publishHashToIpns(ipfsHash) {
 function nodeMayAllowPublish(ipfsClient) {
   // You must have your own IPFS node in order to publish an IPNS name
   // This contains a blacklist of known nodes which do not allow users to publish IPNS names.
-  const nonPublishingNodes = ["ipfs.infura.io"];
+  const nonPublishingNodes = [ 'ipfs.infura.io' ];
   const { host } = ipfsClient.getEndpointConfig();
 
-  return !nonPublishingNodes.some(nodeUrl => {
-    return host.includes(nodeUrl);
-  });
+  return !nonPublishingNodes.some((nodeUrl) => host.includes(nodeUrl));
 }
 
 (async function () {
-  console.log("🛰  Sending to IPFS...");
+  console.log('🛰  Sending to IPFS...');
   console.log();
 
-  const { cid } = await pushDirectoryToIpfs("./build");
+  const { cid } = await pushDirectoryToIpfs('./build');
   if (!cid) {
-    console.log(`📡 App deployment failed`);
+    console.log('📡 App deployment failed');
     return false;
   }
 
-  console.log(`📡 App deployed to IPFS with hash: ${chalk.cyan(cid.toString())}`);
+  console.log(`📡 App deployed to IPFS with hash: ${ chalk.cyan(cid.toString()) }`);
   console.log();
 
-  let ipnsName = "";
+  let ipnsName = '';
   if (nodeMayAllowPublish(ipfs)) {
-    console.log(`✍️  Publishing /ipfs/${cid.toString()} to IPNS...`);
-    process.stdout.write("   Publishing to IPNS can take up to roughly two minutes.\r");
+    console.log(`✍️  Publishing /ipfs/${ cid.toString() } to IPNS...`);
+    process.stdout.write('   Publishing to IPNS can take up to roughly two minutes.\r');
     ipnsName = (await publishHashToIpns(cid.toString())).name;
     clearLine(process.stdout, 0);
 
     if (!ipnsName) {
-      console.log("   Publishing IPNS name on node failed.");
+      console.log('   Publishing IPNS name on node failed.');
     }
 
-    console.log(`🔖 App published to IPNS with name: ${chalk.cyan(ipnsName)}`);
+    console.log(`🔖 App published to IPNS with name: ${ chalk.cyan(ipnsName) }`);
     console.log();
   }
 
-  console.log("🚀 Deployment to IPFS complete!");
+  console.log('🚀 Deployment to IPFS complete!');
   console.log();
 
-  console.log(`🌐 Use the link${ipnsName ? "s" : ""} below to access your app:`);
+  console.log(`🌐 Use the link${ ipnsName ? 's' : '' } below to access your app:`);
   const link = ipfsGateway + cid.toString();
-  console.log(`   IPFS: ${chalk.cyan(link)}`);
+  console.log(`   IPFS: ${ chalk.cyan(link) }`);
 
   if (ipnsName) {
-    const ipnsUrl = `${ipfsGateway}${ipnsName}`;
-    console.log(`   IPNS: ${chalk.cyan(ipnsUrl)}`);
+    const ipnsUrl = `${ ipfsGateway }${ ipnsName }`;
+    console.log(`   IPNS: ${ chalk.cyan(ipnsUrl) }`);
     console.log();
     console.log(
-      "Each new deployment will have a unique IPFS hash while the IPNS name will always point at the most recent deployment.",
+      'Each new deployment will have a unique IPFS hash while the IPNS name will always point at the most recent deployment.',
     );
     console.log(
-      "It is recommended that you share the IPNS link so that people always see the newest version of your app.",
+      'It is recommended that you share the IPNS link so that people always see the newest version of your app.',
     );
   }
   console.log();
   return true;
-})();
+}());
---
+++ packages/react-app/src/components/WalletButton/wallet-button.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Web3Provider } from '@ethersproject/providers';
+import { Button } from '../styled-components';
+
+function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
+  return (
+    <Button
+      onClick={ () => {
+        if (!provider) {
+          loadWeb3Modal();
+        } else {
+          logoutOfWeb3Modal();
+        }
+      } }
+    >
+      { !provider ? 'Connect Wallet' : 'Disconnect Wallet' }
+    </Button>
+  );
+}
+
+WalletButton.propTypes = {
+  provider: PropTypes.instanceOf(Web3Provider),
+  loadWeb3Modal: PropTypes.func.isRequired,
+  logoutOfWeb3Modal: PropTypes.func.isRequired,
+};
+
+WalletButton.defaultProps = {
+  provider: null,
+};
+
+export default WalletButton;
