62e8878232d9a0b4d7c324d52bc0f4f31527561b
eduardioo <eduard_ioo@yahoo.com>
2021-08-23 15:34:42 +0300

    Fix testing failure #2

--- packages/react-app/src/services/ethers-contract.js
+++ packages/react-app/src/services/ethers-contract.js
@@ -1,13 +1,11 @@
 import { ethers } from 'ethers';
 import Abi from '../abi/freaks-abi.json';
-import Web3Service from './web3-service';
 
 export async function getContract() {
   const deployedAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
   const localhost = 'http://localhost:8545';
   const provider = new ethers.providers.JsonRpcProvider(localhost);
-  const address = Web3Service.getAddress(window.ethereum.selectedAddress);
-  const signer = provider.getSigner(address);
+  const signer = provider.getSigner(window.ethereum.selectedAddress);
 
   return new ethers.Contract(deployedAddress, Abi, signer);
 }
