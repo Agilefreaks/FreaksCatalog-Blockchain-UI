1c634843f91cb2aee391e3b2c9f3a6f725f783b8
eduardioo <eduard_ioo@yahoo.com>
2021-08-24 09:59:06 +0300

    Fix test issue

--- packages/react-app/src/services/ethers-contract.js
+++ packages/react-app/src/services/ethers-contract.js
@@ -1,11 +1,13 @@
 import { ethers } from 'ethers';
 import Abi from '../abi/freaks-abi.json';
+import Web3Service from './web3-service';
 
 export async function getContract() {
   const deployedAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
   const localhost = 'http://localhost:8545';
   const provider = new ethers.providers.JsonRpcProvider(localhost);
-  const signer = provider.getSigner(window.ethereum.selectedAddress);
+  const address = Web3Service.getAddress();
+  const signer = provider.getSigner(address);
 
   return new ethers.Contract(deployedAddress, Abi, signer);
 }
